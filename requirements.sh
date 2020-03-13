0914e21f8e3f983dee63f14d6be81dbd381b1774
param321 <param@iitg.ac.in>
2020-03-13 18:01:39 +0530

    requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+npm install
+npm install --save react-native-gesture-handler
+react-native link react-native-gesture-handler
+npm install --save react-native-reanimated
+react-native link react-native-reanimated
+npm install react-native-datepicker --save
+sudo npm i react-native-floating-action --save
