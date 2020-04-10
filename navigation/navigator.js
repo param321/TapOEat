b752dc2f41293bc177b34f8559dd85050364d5cb
param321 <param@iitg.ac.in>
2020-04-10 15:17:54 +0530

    ignore this

--- navigation/navigator.js
+++ navigation/navigator.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createSwitchNavigator } from 'react-navigation';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+// import { createAppContainer } from 'react-navigation';
+// import { createSwitchNavigator } from 'react-navigation';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import Home from '../Screens/Home/home';
 import Login from '../Screens/Auth/login';
 import SignUp from '../Screens/Auth/Signup';
@@ -9,41 +11,46 @@ import Loading from '../Screens/Loading';
 import Main from '../Screens/Main';
 
 export const Navigator = () => (
-  <AppContainer />
+  <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Login} />
+        <Drawer.Screen name="Notifications" component={SignUp} />
+      </Drawer.Navigator>
+    </NavigationContainer>
 );
-const SwitchNavigator = createSwitchNavigator(
-  {
-    Loading: Loading,
-    Login: Login,
-    SignUp: SignUp,
-    Main: Main,
-    Home: Home
-  },
-  {
-    initialRouteName: 'Loading',
-    headerMode: 'none'
-  }
-)
+// const SwitchNavigator = createSwitchNavigator(
+//   {
+//     Loading: Loading,
+//     Login: Login,
+//     SignUp: SignUp,
+//     Main: Main,
+//     Home: Home
+//   },
+//   {
+//     initialRouteName: 'Loading',
+//     headerMode: 'none'
+//   }
+// )
 
-const AppContainer = createAppContainer(SwitchNavigator);
-// export default App
+// const AppContainer = createAppContainer(SwitchNavigator);
 
-// const AppContainer = createAppContainer(AppNavigator);
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Button
+//         onPress={() => navigation.navigate('Login')}
+//         title="Go to notifications"
+//       />
+//     </View>
+//   );
+// }
 
-// const AppNavigator = createStackNavigator({
-// 	Loading: {
-//     	screen: Loading
-//     },
-//     Login: {
-//         screen: Login
-//     },
-//     Home: {
-//         screen: Home
-//     },
-//     Main: {
-//     	screen: Main
-//     },
-//     SignUp: {
-//     	screen: SignUp
-//     },
-//  });
+// function NotificationsScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Button onPress={() => navigation.goBack()} title="Go back home" />
+//     </View>
+//   );
+// }
+
+// const Drawer = createDrawerNavigator();
