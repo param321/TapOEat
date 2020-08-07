e1c133b4622938153bd70bace24398703ffc633f
Pooja Bhagat <pooja.bhagat2000@gmail.com>
2020-08-07 23:59:59 +0530

    Updating home screen

--- Screens/Home/home.js
+++ Screens/Home/home.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { firebaseAuth } from '../../environment/config';
 import { Alert, Platform, Image, View, Text, StyleSheet, Button } from 'react-native';
+// import RecycleTestComponent from './restaurantView.js'
+import firestore from '@react-native-firebase/firestore'
+
+
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentUser: null }
+    this.state = { currentUser: null,
+    restaurantData: null, }
   }
+  async getRestaurantData() {
+    console.log("I am getting data");
+    var snapshot = await firestore()
+    .collection('restaurants').get();
+    console.log("I got data");
+    var restaurantData = [];
+    snapshot.forEach((doc) => {
+      restaurantData.push(doc.data())
+    });
+    console.log(restaurantData);
+  }
+
   componentDidMount() {
     const { currentUser } = firebaseAuth;
-    this.setState({ currentUser })
+    this.setState({ currentUser });
+    this.getRestaurantData();
+    console.log("Yay")
   }
   render() {
     const { currentUser } = this.state
@@ -26,5 +45,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  recyclerview: {
+    flex: 1,
   }
 })
